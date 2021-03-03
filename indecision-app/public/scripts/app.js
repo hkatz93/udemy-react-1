0fe1779df8927d842d719eb3538fa57317879dfe
hkatz <hkatz93@yahoo.com>
2021-03-03 14:44:20 -0500

    working template, no babel

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,7 @@
+console.log('app.js is running');
+
+//var template = <p>This is my JSX template</p>;
+var template = /*#__PURE__*/React.createElement("p", null, "this is my template");
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
