091d97ab4ffb792d8f91988f3cf0131cfeef2fd8
hkatz <hkatz93@yahoo.com>
2021-03-04 14:27:39 -0500

    playground: arrow functions

---
+++ indecision-app/src/playground/es6-arrow-function.js
@@ -0,0 +1,19 @@
+// challenge get first name as arrow function
+
+
+const fullName = 'Howard2 Katz';
+
+//let firstName = (fullName) => {
+//    return fullName.split(' ')[0];
+//};
+
+// short syntax
+let firstName = (fullName) => fullName.split(' ')[0];
+
+
+function getFirstName(fullName) {
+    return fullName.split(' ')[0];
+}
+console.log('full name = '+fullName+', first name = '+firstName(fullName));
+
+console.log('full name = '+fullName+', first name = '+getFirstName(fullName));
