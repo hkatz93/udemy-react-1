a4bf6deb7173c4a73bb9c8bc138bd3e98bd34345
hkatz <hkatz93@yahoo.com>
2021-03-04 09:54:25 -0500

    use JSX for template variable, no var substitions yet

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,15 @@
 console.log('app.js is running');
 
-var template = <p>This is my JSX template</p>;
+var template = (
+    <div>
+    <h1>Howard's page</h1>
+    <p>This part of my JSX template!!!</p>
+    <ol>
+    <li>Item A</li>
+    <li>Item B</li>
+    </ol>
+    </div>
+);
 //var template = /*#__PURE__*/React.createElement("p", null, "this is my template");
 var appRoot = document.getElementById('app');
 
