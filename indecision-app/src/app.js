8274f6085dd46130296eba6be6825d98a8c31236
hkatz <hkatz93@yahoo.com>
2021-03-04 14:11:45 -0500

    var to let/const

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,27 +1,27 @@
 console.log('app.js is running');
 
 // JSX template with title/subtite using JSON object
-var book = {
+let book = {
     title: 'Splat',
     subtitle: 'How High Altitude Fears Changed My Life',
-    options: ['One', 'Two']
+    options: []
 }
 
 
 // subtitle only if exists
 // display options only if length > 0
-var template1 = (
+let template1 = (
     <div>
     <h1>{book.title}</h1>
     {book.subtitle && <p>{book.subtitle}</p>}
     {book.options.length > 0 
-        && 
+        ? 
         <ol>
             {book.options.map((value, index) => {
                 return <li key={index}>{value}</li>
-
             })}
         </ol>
+        : "No options"
     }
     </div>
 );
@@ -32,11 +32,11 @@ var template1 = (
 // p -> age
 // p -> location
 
-var userName =  'howard katz';
-var age = 45;
-var location2 = 'my house2'; // location causes redirect
+const userName =  'howard katz';
+const age = 45;
+const location2 = 'my house2'; // location causes redirect
 
-var user = {
+const user = {
     name: 'Howard Katz',
     age: 45,
     location: 'venice2'
@@ -44,19 +44,19 @@ var user = {
 
 
 //var fLocation = () => 'fLocation';
-var getLocation = (location) => {
+let getLocation = (location) => {
     if (location) {
         return <p>{location}</p>;
     }
 }
 
-var template2 = (
+let template2 = (
 <div>
   <h1>{user.name ?  user.name : 'Anonymous'}</h1>
   {user.age > 18 && <p>Age: {user.age}</p>}
   {getLocation(user.location)}
 </div>
 );
-var appRoot = document.getElementById('app');
+let appRoot = document.getElementById('app');
 
 ReactDOM.render(template1, appRoot);
