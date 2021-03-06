c45434b49e31a891474c997d2b66c0b39a8730c6
hkatz <hkatz93@yahoo.com>
2021-03-05 20:34:44 -0500

    components, static page display

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,13 +1,57 @@
 class Header extends React.Component {
     render() {
-        return <h1>Header</h1>;
+        return (
+            <div>
+            <h1>Indecision Picker</h1>
+            <h2>Put your life in the hands of a computer</h2>
+            </div>
+        );
+    }
+}
+
+class Action extends React.Component {
+    render() {
+        return (
+        <div>
+            <button>What should I do?</button>
+        </div>
+        );
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        return (
+            <div>/>
+
+            <ol>
+                <li>1111</li>
+                <li>2222</li>
+            </ol>
+            </div>
+        );
+    }
+}
+
+class AddOptions extends React.Component {
+    render() {
+        return (
+        <div>
+        <form>
+        <input type='text' name='option' /><button>Add Option</button>
+        </form>
+        </div>
+        );
     }
 }
 
 const jsx = (
+
     <div>
         <Header />
-        <p>Start</p>
+        <Action />
+        <Options />
+        <AddOptions />
     </div>
 );
 
