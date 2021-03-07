5d10fb18d9696c60559af1cba7a498ed183ff6f2
hkatz <hkatz93@yahoo.com>
2021-03-07 09:36:29 -0500

    visibility toggle w/state

--- indecision-app/src/playground/visibility-toggle.js
+++ indecision-app/src/playground/visibility-toggle.js
@@ -1,27 +1,59 @@
-console.log('app.js is running');
+// Challenge -> create components and use state
 
-const app = {
-    title: 'Visibility Toggle',
-    details: 'These are the details',
-}
+// console.log('app.js is running');
 
-let visibility = false;
-
-const toggleVisibility = () => {
-    visibility = !visibility;
-    render();
-}
-
-const render = () => {
-
-    const template = (
+class MessageToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'message': 'This is my message',
+            'visibility': false
+        };
+        this.toggleVisibility = this.toggleVisibility.bind(this);
+    }
+    toggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                'visibility': !prevState.visibility
+            };
+            
+        })
+    }
+    render() {
+        return (
         <div>
-        <h1>{app.title}</h1>
-        <button onClick={toggleVisibility}>{visibility ? 'Hide details' : 'Show Details'}</button>
-        {visibility && <p>{app.details}</p>}
-        </div>
-        
-    );
-    ReactDOM.render(template, document.getElementById('app'));
+        <h1>{this.state.message}</h1>
+        <button onClick={this.toggleVisibility}>{this.state.visibility ? 'Hide details' : 'Show Details'}</button>
+        {this.state.visibility && <p>{this.state.message}</p>}
+        </div>       
+        );
+    }
 }
-render();
+
+ReactDOM.render(<MessageToggle />, document.getElementById('app'));
+
+// const app = {
+//     title: 'Visibility Toggle',
+//     details: 'These are the details',
+// }
+
+// let visibility = false;
+
+// const toggleVisibility = () => {
+//     visibility = !visibility;
+//     render();
+// }
+
+// const render = () => {
+
+//     const template = (
+//         <div>
+//         <h1>{app.title}</h1>
+//         <button onClick={toggleVisibility}>{visibility ? 'Hide details' : 'Show Details'}</button>
+//         {visibility && <p>{app.details}</p>}
+//         </div>
+        
+//     );
+//     ReactDOM.render(template, document.getElementById('app'));
+// }
+// render();
