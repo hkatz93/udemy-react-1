49d17227da49d6a4f314bf3b50abcfac059f437f
hkatz <hkatz93@yahoo.com>
2021-03-11 18:22:42 -0500

    import & export w/default

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,13 @@
+//import './utils';
+//import { square, add } from './utils';
+import isSenior, {isAdult, canDrink} from './person';
+
 console.log('webpack running!');
+// console.log('square(4)=', square(4));
+// console.log('add(3,40)=', add(3,40));
+console.log('isAdult(13)=', isAdult(13));
+console.log('isAdult(18)=', isAdult(18));
+console.log('canDrink(18)=', canDrink(18));
+console.log('canDrink(21)=', canDrink(21));
+console.log('isSenior(12)=', isSenior(12));
+console.log('isSenior(65)=', isSenior(65));
---
+++ indecision-app/src/utils.js
@@ -0,0 +1,6 @@
+console.log('utils.js is running');
+
+export const square = (x) => x * x;
+export const add = (x, y) => x + y; 
+
+//export { square, add }; // long method
