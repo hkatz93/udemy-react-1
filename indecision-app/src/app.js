3e1840ff6a2eedf1afaabd3b5b3fcc94a0357b7b
hkatz <hkatz93@yahoo.com>
2021-03-12 13:58:51 -0500

    ind: remove unneeded imports

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Option from './components/Option';
-import Options from './components/Options';
-import AddOption from './components/AddOption';
-import Action from './components/Action';
-import Header from './components/Header';
+
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
