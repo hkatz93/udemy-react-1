20b19c9599ceafa4114a8005f1bc75df1ecd815c
hkatz <hkatz93@yahoo.com>
2021-03-21 18:15:10 -0400

    modal onRequestClose to close w/out button

--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -4,6 +4,7 @@ import Modal from 'react-modal';
 const OptionModal = (props) => (
   <Modal
     isOpen={!!props.selectedOption}
+    onRequestClose={props.handleDeleteOption}
     contentLabel='Selected Option'
   >
   <h1>Selected Option</h1>
